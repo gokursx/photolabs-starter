134a3a1d26162c7d203ad35da5bc3601ced263a1
Gokul Krishna <gokursx.123@gmail.com>
2024-06-17 16:53:32 -0700

    add PhotoFavButton component to PhotoListItem component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,13 +3,11 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-const [favouritePhoto, setFavouritePhoto] = useState(false);
-
-<div className="photo-list__fav-icon" onClick={() => setFavouritePhoto(prev => !prev)}></div>
-
 function PhotoFavButton() {
+  const [favouritePhoto, setFavouritePhoto] = useState(false);
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={() => setFavouritePhoto(prev => !prev)}>
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
       </div>
