f1bb1bb505c3b150e416a962ff4352f9626c5950
Gokul Krishna <gokursx.123@gmail.com>
2024-06-17 16:49:36 -0700

    add a state variable to PhotoFavButton component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,6 +3,10 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+const [favouritePhoto, setFavouritePhoto] = useState(false);
+
+<div className="photo-list__fav-icon" onClick={() => setFavouritePhoto(prev => !prev)}></div>
+
 function PhotoFavButton() {
   return (
     <div className="photo-list__fav-icon">
