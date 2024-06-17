30f6053259287b17804c9487e0752394b7cd1aba
Gokul Krishna <gokursx.123@gmail.com>
2024-06-17 16:13:10 -0700

    wrap user name and location in user-info

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,11 +7,13 @@ function PhotoListItem({ photo }) {
       <div className="photo-list__item-content">
         <img className="photo-list__item-image" src={photo.imageSource} alt="Photo" /> 
       </div>
-      <div className="photo-list__user_details">
+      <div className="photo-list__user-details">
+      <div className="photo-list__user-info">
         <img className="photo-list__user-image" src={photo.profile} alt={photo.username} /> 
         <h3 className="photo-list__user-name">{photo.username}</h3>
         {/* Displaying location */}
         <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+        </div>
       </div>
     </div>
   );
