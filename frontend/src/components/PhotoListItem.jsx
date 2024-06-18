63d74d29e82a2ff86288c4da8824d7f4a00ca34f
Gokul Krishna <gokursx.123@gmail.com>
2024-06-17 17:13:26 -0700

    import PhotoFavButton in PhotoListItem.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from './PhotoFavButton';
 
 function PhotoListItem({ photo }) {
   return (
     <div className="photo-list__item">
       <div className="photo-list__item-content">
-        <img className="photo-list__item-image" src={photo.imageSource} alt="Photo" /> 
+        <img className="photo-list__item-image" src={photo.imageSource} alt="Photo" />
+        <PhotoFavButton />
       </div>
       <div className="photo-list__user-details">
-        <img className="photo-list__user-image" src={photo.profile} alt={photo.username} /> 
+        <img className="photo-list__user-image" src={photo.profile} alt={photo.username} />
         <div className="photo-list__user-info">
-        <h3 className="photo-list__user-name">{photo.username}</h3>
-        {/* Displaying location */}
-        <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+          <h3 className="photo-list__user-name">{photo.username}</h3>
+          {/* Displaying location */}
+          <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
         </div>
       </div>
     </div>
