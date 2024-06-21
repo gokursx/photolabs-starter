bce277008c402795aefd93c2ff67ddf5c50f14a9
Gokul Krishna <gokursx.123@gmail.com>
2024-06-21 13:48:15 -0700

    add useReducer to useApplicationData

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -4,11 +4,10 @@ import { useReducer } from "react";
 export const ACTIONS = {
   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
-  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
   SELECT_PHOTO: 'SELECT_PHOTO',
-  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
-}
+  CLOSE_PHOTO_DETAILS: 'CLOSE_PHOTO_DETAILS'
+};
 
 const initialState = {
   isModalVisible: false,
@@ -27,11 +26,7 @@ const useApplicationData = () => {
 
   // Function to update favorite photo IDs
   const updateToFavPhotoIds = (id) => {
-    if (state.favPhotoIds.includes(id)) {
-      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: { id } });
-    } else {
-      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { id } });
-    }
+    dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { id } });
   };
 
   // Function to load topics
@@ -41,58 +36,50 @@ const useApplicationData = () => {
 
   // Function to close the photo details modal
   const onClosePhotoDetailsModal = () => {
-    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS });
+    dispatch({ type: ACTIONS.CLOSE_PHOTO_DETAILS });
   };
-
+  
   function reducer(state, action) {
     switch (action.type) {
       case ACTIONS.FAV_PHOTO_ADDED:
         if (state.favPhotoIds.includes(action.payload.id)) {
           return state; // No need to update, the photo ID is already in the favorites
         }
-        return { 
+        return {
           ...state,
           favPhotoIds: [...state.favPhotoIds, action.payload.id]
         };
-        
+
       case ACTIONS.FAV_PHOTO_REMOVED:
         return {
           ...state,
           favPhotoIds: state.favPhotoIds.filter(id => id !== action.payload.id)
         };
-  
-      case ACTIONS.SET_PHOTO_DATA:
-        return {
-          ...state,
-          selectedPhoto: action.payload.photo,
-          isModalVisible: true
-        };
-  
+
       case ACTIONS.SET_TOPIC_DATA:
         return {
           ...state,
           topics: action.payload.topics
         };
-  
+
       case ACTIONS.SELECT_PHOTO:
         return {
           ...state,
           selectedPhoto: action.payload.photo,
           isModalVisible: true
         };
-  
-      case ACTIONS.DISPLAY_PHOTO_DETAILS:
+
+      case ACTIONS.CLOSE_PHOTO_DETAILS:
         return {
           ...state,
           isModalVisible: false,
           selectedPhoto: null
         };
-  
+
       default:
         throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
     }
   }
-
   return {
     state,
     onPhotoSelect,
