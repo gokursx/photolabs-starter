d91ae0f8b9646949f16844a1a90c0b8687c65a59
Gokul Krishna <gokursx.123@gmail.com>
2024-06-21 13:24:24 -0700

    add useReducer to useApplicationData

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,5 +1,28 @@
 import { useState } from "react";
 
+/* insert app levels actions below */
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case FAV_PHOTO_ADDED:
+      return { /* insert logic */ }
+    { /* insert all relevant actions as case statements*/ }  
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
 const useApplicationData = () => {
   const [state, setState] = useState({
     isModalVisible: false,
